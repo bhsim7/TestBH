ebd04883328a371f1ff357ce71033829219982d0
bhsim7 <newnewlife7@gmail.com>
2021-01-19 13:28:33 +0900

    3rd change

--- javaTest/src/javaTest/TestBh.java
+++ javaTest/src/javaTest/TestBh.java
@@ -5,6 +5,7 @@ public class TestBh {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Java Test");
+		System.out.println("This is bhsim7");   //2021-01-19
 	}
 
 }
